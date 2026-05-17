3866047ae88a1af5753dfd73028445e39478d546
vomiter-scp-zh <97169118+vomiter-scp-zh@users.noreply.github.com>
2026-05-18 05:45:12 +0800

    0.8.2 clear left over codes

--- src/main/java/com/vomiter/mobsuseshields/mixin/LivingEntity_ShieldBlockEffectsMixin.java
+++ src/main/java/com/vomiter/mobsuseshields/mixin/LivingEntity_ShieldBlockEffectsMixin.java
@@ -98,9 +98,4 @@ public abstract class LivingEntity_ShieldBlockEffectsMixin extends Entity {
             }
         }
     }
-
-    @Inject(method = "tick", at = @At("TAIL"))
-    private void mus$syncBodyRotWhileShielding(CallbackInfo ci) {
-
-    }
 }
